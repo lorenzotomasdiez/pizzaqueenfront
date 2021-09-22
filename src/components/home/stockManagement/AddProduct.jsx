c3638e196f6c987cacab9e55e38cc7eff83812f4
Lorenzo Tomas Diez <lorenzotomasdiez@github.com>
2021-09-22 00:21:48 -0300

    Stock list

--- src/components/home/stockManagement/AddProduct.jsx
+++ src/components/home/stockManagement/AddProduct.jsx
@@ -1,12 +1,76 @@
+import { Button, InputLabel, NativeSelect, TextField } from '@material-ui/core'
+import { useForm } from 'react-hook-form'
 import React from 'react'
 
-const AddProduct = () => {
+const AddProduct = ({setOpenPopup}) => {
+    const {register, formState: { errors }, handleSubmit , setValue} = useForm()
+    
+    const onSubmit = (data, e) => {
+        e.target.reset()
+        console.log(data)
+        setOpenPopup(false)
+    }
+
+    const onChangeSelect = (data) => {
+        setValue('productCategory', data.target.value)
+    }
+
     return (
-        <div>
-            <form>
-                add product
-            </form>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="addproduct_form">
+                <div className="addproduct_child">
+                    <div className="addproduct_fields">
+                        <TextField
+                            {...register('productName',{ required: true})}
+                            label="Nombre"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="outlined"
+                            />
+                        <p className="validerror">{errors.productName && "Debe ingresar nombre"}</p>
+                    </div>
+                    <div className="addproduct_fields">
+                        <InputLabel htmlFor="select">Categoria</InputLabel>
+                        <NativeSelect id="select" onChange={onChangeSelect} >
+                            <option value="Pizza">Pizza</option>
+                            <option value="Bebida">Bebida</option>
+                            <option value="Insumos">Insumos</option>
+                            <option value="Otros">Otros</option>
+                        </NativeSelect>
+                    </div>
+                </div>
+                <div className="addproduct_child">
+                    <div className="addproduct_fields">
+                        <TextField
+                            {...register('productPrice',{ required: true})}
+                            label="Precio"
+                            type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="outlined"
+                            />
+                        <p className="validerror">{errors.productPrice && "Debe ingresar Precio"}</p>
+                    </div>
+                    <div className="addproduct_fields">
+                        <TextField
+                            {...register('productStock',{ required: true})}
+                            label="Stock"
+                            type="number"
+                            defaultValue={0}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="outlined"
+                            />
+                    </div>
+                </div>
+                <div>
+                    <Button type="submit" variant="contained" className="add_product_button">Agregar Producto</Button>
+                </div>
+            </div>
+        </form>
     )
 }
 
--- src/components/home/stockManagement/StockManagement.jsx
+++ src/components/home/stockManagement/StockManagement.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button } from '@material-ui/core'
+import React, { useState } from 'react'
+import PopUp from '../orderManagement/PopUp'
 import AddProduct from './AddProduct'
+import List from './List'
+import "./style.css"
 const StockManagement = () => {
+    const [openAddProduct, setOpenAddProduct] = useState(false)
     return (
         <div>
-            <AddProduct />
+            <div className="stock_header">
+                <Button
+                    variant="contained"
+                    onClick={()=>{setOpenAddProduct(true)}}
+                >Agregar Producto
+                </Button> 
+            </div>
+            <div className="stock_content">
+                <List />
+            </div>
+            <PopUp
+                openPopup={openAddProduct}
+                setOpenPopup={setOpenAddProduct}
+                title="Agregar Pedido"
+                className="popup"
+            >
+                <AddProduct setOpenPopup={setOpenAddProduct} />
+            </PopUp>
         </div>
     )
 }
